de1603fcd30cecd7af25c8154191341701f32752
ClaudiaGugiuman <claudiagugiuman@yahoo.com>
2021-07-24 07:26:34 +0200

    C:/Program Files/Git/medicalRecord endpoint

--- SafetyFirstApp/src/main/java/com/safetyfirst/SafetyFirstApp/repository/IMedicalRecordsProxy.java
+++ SafetyFirstApp/src/main/java/com/safetyfirst/SafetyFirstApp/repository/IMedicalRecordsProxy.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface IMedicalRecordsProxy {
     public List<MedicalRecord> readMedicalRecords();
+    
+    public List<MedicalRecord> deleteMedicalRecord(String firstName, String lastName);
+    
+    public List<MedicalRecord> modifyMedicalRecord(String firstName, String lastName, List<String> newMedications, List<String> newAllergies);
+    
+    public List<MedicalRecord> addMedicalRecord(MedicalRecord medicalRecord);
 }
--- SafetyFirstApp/src/main/java/com/safetyfirst/SafetyFirstApp/repository/MedicalRecordsProxy.java
+++ SafetyFirstApp/src/main/java/com/safetyfirst/SafetyFirstApp/repository/MedicalRecordsProxy.java
@@ -19,4 +19,46 @@ public class MedicalRecordsProxy implements IMedicalRecordsProxy {
         System.out.println("medical records list is " + medicalRecords);
         return medicalRecords;
     }
+    
+    @Override
+    public List<MedicalRecord> deleteMedicalRecord(String firstName, String lastName) {
+        List<MedicalRecord> medicalRecords = recoveredData.getMedicalrecords();
+        MedicalRecord medicalRecordToDelete = null;
+        for (MedicalRecord m : medicalRecords) {
+            if (m.getFirstName().equals(firstName) && m.getLastName().equals(lastName)) {
+                medicalRecordToDelete = m;
+            }
+        }
+        medicalRecords.remove(medicalRecordToDelete);
+        return medicalRecords;
+    }
+    
+    @Override
+    public List<MedicalRecord> modifyMedicalRecord(String firstName, String lastName, List<String> newMedications, List<String> newAllergies) {
+        List<MedicalRecord> medicalRecordList = recoveredData.getMedicalrecords();
+        for (MedicalRecord m : medicalRecordList) {
+            List<String> medications = m.getMedications();
+            List<String> allergies = m.getAllergies();
+            if (m.getFirstName().equals(firstName) && m.getLastName().equals(lastName)) {
+                
+                for (String newMedication : newMedications) {
+                    medications.add(newMedication);
+                }
+                
+                for (String newAllergie : newAllergies) {
+                    allergies.add(newAllergie);
+                }
+            }
+            
+        }
+        return medicalRecordList;
+    }
+    
+    @Override
+    public List<MedicalRecord> addMedicalRecord(MedicalRecord medicalRecord) {
+        List<MedicalRecord> medicalRecords = recoveredData.getMedicalrecords();
+        medicalRecords.add(medicalRecord);
+        return medicalRecords;
+    }
+    
 }
